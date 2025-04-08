c3810c576e96b287e281feb6317d0bd199461f9a
Sarika Kalsait <71684198+sarikakalsait@users.noreply.github.com>
2025-04-08 18:55:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,8 @@
-provider "aws" {
-  region = "ap-south-1"
-}
- backend "s3" {
-    bucket = "tf-tuts-state-hi"
-    key    = "terraform/state/terraform.tfstate"
-    region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
 }
